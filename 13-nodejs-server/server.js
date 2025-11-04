9aa47656bfee3b2b291c4ff99f2e58e6d8433fb2
JavierManzano-30 <javier.manzanooliveros@adaits.es>
2025-11-04 20:00:10 +0100

    12 changes

---
+++ 13-nodejs-server/server.js
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World!</h1>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
